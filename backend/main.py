13659290dc3c76383bd9f5ec17d87eb9842ac910
Larry <lukaoma@utexas.edu>
2020-10-19 00:36:27 -0700

    update to do fetch

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ CORS(app)
 def index(path):
   return render_template("index.html")
 
+@app.route("/api/test")
+def test():
+      return "TEST"
 
 if __name__ == "__main__":
-    # app.run(host='0.0.0.0', port=5000, threaded=True)
     app.run(host='0.0.0.0', port=80, threaded=True, debug=True)
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import useAxios from 'axios-hooks'
 
 function App() {
+
+const[{data}]= useAxios("/api/test");
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,7 +20,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React {data}
         </a>
       </header>
     </div>
